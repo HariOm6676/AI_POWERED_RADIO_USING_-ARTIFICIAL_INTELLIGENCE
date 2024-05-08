cf46775a7e43f3e49024c1b2d24a3200107ae1ce
HariOm6676 <118880027+HariOm6676@users.noreply.github.com>
2024-05-09 03:54:45 +0530

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,27 @@
-import "package:flutter/material.dart";
-import "package:flutter_application_1/pages/home_page.dart";
-import "package:google_fonts/google_fonts.dart";
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/pages/home_page.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-void main(){
+void main() {
   runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key); // Correct constructor syntax
 
   @override
   Widget build(BuildContext context) {
+    print("inside my app");
     return MaterialApp(
       title: "Flutter Demo",
       theme: ThemeData(
         fontFamily: GoogleFonts.poppins().fontFamily,
       ),
       debugShowCheckedModeBanner: false,
-      home: const HomePage(),
+      initialRoute: '/home',
+      routes: {
+        '/home': (context) => HomePage(),
+      },
     );
-}
+  }
 }
